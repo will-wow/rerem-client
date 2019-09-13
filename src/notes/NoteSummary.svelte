a7da8288bb7674007a9f79cbed616a9eacd26eb7
Will Ockelmann-Wagner <will.ockelmann.wagner@gmail.com>
2019-09-13 16:46:33 -0700

    show titles

--- src/notes/NoteSummary.svelte
+++ src/notes/NoteSummary.svelte
@@ -1,8 +1,12 @@
 <script>
   export let note;
   export let onClick;
+
+  $: text = note.body || note.id
+
+  $: title = text.split("\n")[0]
 </script>
 
 <div class="note-summary">
-  <button on:click={onClick}>{note.body || note.id}</button>
+  <button on:click={onClick}>{title}</button>
 </div>
